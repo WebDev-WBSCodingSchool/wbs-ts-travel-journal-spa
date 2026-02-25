3d020cbcb02e6cca1edff9291734904c0420a5a8
Garrett Lander <garrett.jlander@gmail.com>
2026-02-25 13:52:18 +0100

    removed unused skeletons

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,4 @@
 import Navbar from './UI/Navbar';
 import PostCard from './Posts/PostCard';
-import PostSkeleton from './UI/PostSkeleton';
-import PostsSkeleton from './UI/PostsSkeleton';
 
-export { Navbar, PostCard, PostSkeleton, PostsSkeleton };
+export { Navbar, PostCard };
