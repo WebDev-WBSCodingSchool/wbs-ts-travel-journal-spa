eebe3ea06f55348911e3efd3dba5165d6e3a987e
Renke Brixel <renke.brixel@gmail.com>
2026-04-14 20:48:53 +0200

    Reapply "- Update TypeScript to v6"
    This reverts commit 65a1e95d859475da69b33a509ec4b842afde80ee.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,14 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-	VITE_APP_TRAVEL_JOURNAL_API_URL: z.url().default('http://localhost:8000'),
-	VITE_APP_AUTH_SERVER_URL: z.url().default('http://localhost:3000/auth')
+  VITE_APP_TRAVEL_JOURNAL_API_URL: z.url().default('http://localhost:8000'),
+  VITE_APP_AUTH_SERVER_URL: z.url().default('http://localhost:3000/auth')
 });
 
 const parsedEnv = envSchema.safeParse(import.meta.env);
 
 if (!parsedEnv.success) {
-	console.error(
-		'❌ Invalid environment variables:\n',
-		z.prettifyError(parsedEnv.error)
-	);
-	process.exit(1);
+  throw new Error(`❌ Invalid environment variables:\n${z.prettifyError(parsedEnv.error)}`);
 }
 
-export const { VITE_APP_TRAVEL_JOURNAL_API_URL, VITE_APP_AUTH_SERVER_URL } =
-	parsedEnv.data;
+export const { VITE_APP_TRAVEL_JOURNAL_API_URL, VITE_APP_AUTH_SERVER_URL } = parsedEnv.data;
